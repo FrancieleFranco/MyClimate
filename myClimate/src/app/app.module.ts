91b9f43e2ae62f22c22e58c1c8f242e35ca723e3
Franciele Franco <fran.alves1108@gmail.com>
2025-03-31 18:15:32 -0300

    create page home

--- myClimate/src/app/app.module.ts
+++ myClimate/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,17 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from 'src/components/footer/footer.component';
+import { HomeComponent } from 'src/pages/home/home.component';
+import { ButtonModule } from 'src/components/button/button.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
