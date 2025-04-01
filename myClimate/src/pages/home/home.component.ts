fd8a3980c5e250ba74c59a83f5cc7ec45f07daa7
Franciele Franco <fran.alves1108@gmail.com>
2025-04-01 17:35:11 -0300

    message style adjustments

--- myClimate/src/pages/home/home.component.ts
+++ myClimate/src/pages/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { WeatherResponse } from 'src/models/weather';
 import { ClimateService } from 'src/service/climateService.service';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   form: FormGroup;
@@ -19,38 +23,32 @@ export class HomeComponent implements OnInit {
   cityFound: string | null | undefined;
   messageTemperature: string = '';
 
-  constructor(private climateService: ClimateService,
-    private fb: FormBuilder) {
+  constructor(private climateService: ClimateService, private fb: FormBuilder) {
     this.form = this.fb.group({
-      city: ['', Validators.required]
+      city: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 
   onSubmit(): void {
     this.getWeatherData();
-
-
   }
 
   getWeatherData(): void {
     const cityName = this.form.get('city')?.value;
 
     if (!cityName) {
-      console.error("Cidade inválida.");
+      console.error('Cidade inválida.');
       return;
     }
 
     this.climateService.getClimate(cityName).subscribe({
       next: (data) => {
-        console.log("Dados,da clima recebido", data);
-          this.resultWeather = data;
-        this.errorMessage = ''
-        console.log(this.resultWeather)
+        console.log('Dados,da clima recebido', data);
+        this.resultWeather = data;
+        this.errorMessage = '';
+        console.log(this.resultWeather);
         this.temperature = this.resultWeather.main.temp;
         this.cityFound = this.resultWeather.name;
 
@@ -58,26 +56,28 @@ export class HomeComponent implements OnInit {
       },
       error: (error) => {
         console.error('Erro ao buscar dados do clima', error);
-        this.errorMessage = 'Cidade não encontrada ou erro na API.';
+        this.errorMessage = 'Cidade não encontrada';
         this.resultWeather = null;
-      }
+      },
     });
   }
   setMessage(): void {
     if (this.temperature !== null) {
       if (this.temperature < 10) {
-        this.messageTemperature = 'Está muito frio! Vista um casaco bem quente. ❄️🧥';
+        this.messageTemperature =
+          'Está muito frio! Vista um casaco bem quente. ❄️🧥';
       } else if (this.temperature >= 10 && this.temperature < 20) {
-        this.messageTemperature = 'O tempo está fresco, melhor levar um agasalho. 🌬️🧣';
+        this.messageTemperature =
+          'O tempo está fresco, melhor levar um agasalho. 🌬️🧣';
       } else if (this.temperature >= 20 && this.temperature < 30) {
-        this.messageTemperature = 'O clima está agradável, aproveite o dia! ☀️😎';
+        this.messageTemperature =
+          'O clima está agradável, aproveite o dia! ☀️😎';
       } else if (this.temperature >= 30 && this.temperature < 40) {
         this.messageTemperature = 'Está bem quente! Beba bastante água. 💦🔥';
       } else {
-        this.messageTemperature = 'Calor extremo! Evite sair no sol e se hidrate bem. 🌡️🥵';
+        this.messageTemperature =
+          'Calor extremo! Evite sair no sol e se hidrate bem. 🌡️🥵';
       }
     }
   }
-
-
 }
